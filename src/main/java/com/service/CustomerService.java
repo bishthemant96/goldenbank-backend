e4703ac94aa1ac0714bbc613e62ea43f8a126c4c
unknown <bharat.gupta@del1-lhp-n65278.synapse.com>
2019-08-19 11:45:19 +0530

    My mehnat

--- src/main/java/com/service/CustomerService.java
+++ src/main/java/com/service/CustomerService.java
@@ -4,4 +4,6 @@ package com.service;
 
 public interface CustomerService {
 
+	//authenticateUser(); takes input as loginModel and sends boolean data as true for correct username password vice-versa.
+	
 }
