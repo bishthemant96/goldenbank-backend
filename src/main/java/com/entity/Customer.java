99cbae7cb719ba2adcea51edb63411725f289dbf
Rishabh Omar <rishabh.omar@synapse.com>
2019-08-19 11:49:55 +0530

    En-tities

---
+++ src/main/java/com/entity/Customer.java
@@ -0,0 +1,153 @@
+package com.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "Customer")
+public class Customer {
+
+	@Id
+	@Column(name = "cusID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int cusID;
+
+	@Column(name = "cusName")
+	private String cusName;
+
+	@Column(name = "cusPassword")
+	private String cusPassword;
+
+	@Column(name = "cusAge")
+	private String cusAge;
+
+	@Column(name = "cusGender")
+	private String cusGender;
+
+	@Column(name = "cusNationality")
+	private int cusNationality;
+
+	@Transient
+	private List<String> cusIDProof; // will take input from form but not saving in database
+
+	@Column(name = "cusNo")
+	private long cusNo;
+
+	@Column(name = "cusEmail")
+	private String cusEmail;
+
+	@Column(name = "empJoiningDate")
+	private String empJoiningDate;
+
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	List<LoanAccount> loanAccount;
+
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	List<Account> account;
+
+	public int getCusID() {
+		return cusID;
+	}
+
+	public void setCusID(int cusID) {
+		this.cusID = cusID;
+	}
+
+	public String getCusName() {
+		return cusName;
+	}
+
+	public void setCusName(String cusName) {
+		this.cusName = cusName;
+	}
+
+	public String getCusPassword() {
+		return cusPassword;
+	}
+
+	public void setCusPassword(String cusPassword) {
+		this.cusPassword = cusPassword;
+	}
+
+	public String getCusAge() {
+		return cusAge;
+	}
+
+	public void setCusAge(String cusAge) {
+		this.cusAge = cusAge;
+	}
+
+	public String getCusGender() {
+		return cusGender;
+	}
+
+	public void setCusGender(String cusGender) {
+		this.cusGender = cusGender;
+	}
+
+	public int getCusNationality() {
+		return cusNationality;
+	}
+
+	public void setCusNationality(int cusNationality) {
+		this.cusNationality = cusNationality;
+	}
+
+	public List<String> getCusIDProof() {
+		return cusIDProof;
+	}
+
+	public void setCusIDProof(List<String> cusIDProof) {
+		this.cusIDProof = cusIDProof;
+	}
+
+	public long getCusNo() {
+		return cusNo;
+	}
+
+	public void setCusNo(long cusNo) {
+		this.cusNo = cusNo;
+	}
+
+	public String getCusEmail() {
+		return cusEmail;
+	}
+
+	public void setCusEmail(String cusEmail) {
+		this.cusEmail = cusEmail;
+	}
+
+	public String getEmpJoiningDate() {
+		return empJoiningDate;
+	}
+
+	public void setEmpJoiningDate(String empJoiningDate) {
+		this.empJoiningDate = empJoiningDate;
+	}
+
+	public List<LoanAccount> getLoanAccount() {
+		return loanAccount;
+	}
+
+	public void setLoanAccount(List<LoanAccount> loanAccount) {
+		this.loanAccount = loanAccount;
+	}
+
+	public List<Account> getAccount() {
+		return account;
+	}
+
+	public void setAccount(List<Account> account) {
+		this.account = account;
+	}
+
+}
---
+++ src/main/java/com/entity/Employee.java
@@ -0,0 +1,115 @@
+package com.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+	@Id
+	@Column(name = "empID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int empID;
+
+	@Column(name = "empName")
+	private String empName;
+
+	@Column(name = "empPassword")
+	private String empPassword;
+
+	@Column(name = "empBranch")
+	private String empBranch;
+
+	@Column(name = "empPost")
+	private String empPost;
+
+	@Column(name = "empAge")
+	private int empAge;
+
+	@Column(name = "empNo")
+	private long empNo;
+
+	@Column(name = "empEmail")
+	private String empEmail;
+
+	@Column(name = "empJoiningDate")
+	private String empJoiningDate;
+
+	public int getEmpID() {
+		return empID;
+	}
+
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpPassword() {
+		return empPassword;
+	}
+
+	public void setEmpPassword(String empPassword) {
+		this.empPassword = empPassword;
+	}
+
+	public String getEmpBranch() {
+		return empBranch;
+	}
+
+	public void setEmpBranch(String empBranch) {
+		this.empBranch = empBranch;
+	}
+
+	public String getEmpPost() {
+		return empPost;
+	}
+
+	public void setEmpPost(String empPost) {
+		this.empPost = empPost;
+	}
+
+	public int getEmpAge() {
+		return empAge;
+	}
+
+	public void setEmpAge(int empAge) {
+		this.empAge = empAge;
+	}
+
+	public long getEmpNo() {
+		return empNo;
+	}
+
+	public void setEmpNo(long empNo) {
+		this.empNo = empNo;
+	}
+
+	public String getEmpEmail() {
+		return empEmail;
+	}
+
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+
+	public String getEmpJoiningDate() {
+		return empJoiningDate;
+	}
+
+	public void setEmpJoiningDate(String empJoiningDate) {
+		this.empJoiningDate = empJoiningDate;
+	}
+
+}
