2979aceb5a178704f82eda120301e112adb12959
Hemant Singh Bisht <bishthemant96@gmail.com>
2019-08-19 16:52:20 +0530

    Minor changes

--- src/main/java/com/service/EmployeeService.java
+++ src/main/java/com/service/EmployeeService.java
@@ -1,10 +1,12 @@
 package com.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dao.EmployeeDao;
+import com.entity.Employee;
 import com.model.EmployeeModel;
 
 @Transactional
@@ -14,8 +16,10 @@ public class EmployeeService {
 	@Autowired
 	EmployeeDao eDao;	
 	
-	EmployeeModel authentication(EmployeeModel employee) {
-		EmployeeEntity empEntity = eDao.readEmployee(employee);
-		return null;
+	public EmployeeModel authentication(EmployeeModel employee) {
+		Employee empEntity = new Employee();
+		BeanUtils.copyProperties(employee, empEntity);
+		empEntity = eDao.readEmployee(empEntity);
+		return employee;
 	}
 }
