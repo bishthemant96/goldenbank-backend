ccea0c36105ff660e4abf8171eb2a4eaaa18d130
Rishabh Omar <rishabh.omar@synapse.com>
2019-08-20 12:21:58 +0530

    creation of customer controller

--- src/main/java/com/service/CustomerService.java
+++ src/main/java/com/service/CustomerService.java
@@ -1,21 +1,56 @@
 package com.service;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 
+import com.dao.CustomerDao;
+import com.entity.CustomerEntity;
 import com.model.CustomerModel;
 
-public interface CustomerService {
-	
-	//return bool value 
-	public boolean authenticateUser(String username, String password);
+@Transactional
+@Service
+public class CustomerService {
 
-	//return in console: addition successful
-	//return a javascript popup saying customer added sucessfully to dashboard
-	public boolean addCustomer(CustomerModel customerModel);
+	@Autowired
+	CustomerDao cDao;
 
-	public boolean deleteCustomer(int empId);
+	/*
+	 * public boolean authenticateUser(CustomerModel cModel) { CustomerEntity
+	 * customer = new CustomerEntity();
+	 * 
+	 * BeanUtils.copyProperties(cModel, customer);
+	 * 
+	 * customer = cDao.readCustomer(customer);
+	 * 
+	 * BeanUtils.copyProperties(employee, empEntity); empEntity =
+	 * eDao.readEmployee(empEntity);
+	 * 
+	 * 
+	 * return false; }
+	 */
 
-	public void updateCustomer(Model model);
-	
-	
+	public boolean addCustomer(CustomerModel customerModel) {
+		CustomerEntity customer = new CustomerEntity();
+		BeanUtils.copyProperties(customerModel, customer);
+		try {
+			customer = cDao.createCustomer(customer);
+			return true;
+		} catch (Exception e) {
+			System.out.println("Error occured while Adding customer...");
+			return false;
+		}
+	}
+
+	public boolean deleteCustomer(int empId) {
+		return false;
+
+	}
+
+	public void updateCustomer(Model model) {
+
+	}
+
 }
