3c3fae489031a13e2c70218cc943c6ca5d98dba2
unknown <bharat.gupta@del1-lhp-n65278.synapse.com>
2019-08-20 12:01:49 +0530

    Minor changes

--- src/main/java/com/controller/CustomerController.java
+++ src/main/java/com/controller/CustomerController.java
@@ -2,17 +2,59 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.model.CustomerModel;
+import com.service.CustomerService;
 
 //This controller will handle the customer CRUD in customer management
 @Controller
 public class CustomerController {
-	
-	@RequestMapping(value="/", method = RequestMethod.GET)
+
+	CustomerModel customerModel;
+	CustomerService customerService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getPage(Model model) {
 		return "login";
+	}
+
+	// customerManagement/addCustomer
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String addCustomer(@ModelAttribute("customerModel") Model model) {
+		boolean isAdded = customerService.addCustomer(customerModel);
+
+		if (isAdded) {
+			model.addAttribute("addMessage", "Customer Sucessfully added");
+		} else {
+			model.addAttribute("addMessage", "Error in adding customer");
+		}
+
+		return "dashboard";
+
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String deleteCustomer(int id, Model model) {
+
+		boolean isDeleted = customerService.deleteCustomer(id);
+
+		if (isDeleted) {
+			model.addAttribute("deleteMessage", "Customer Sucessfully added");
+		} else {
+			model.addAttribute("deleteMessage", "Error in adding customer");
+		}
+
+		return "dashboard";
+	}
+
+	@RequestMapping(value = "/customerManagement/updateCustomer", method = RequestMethod.POST)
+	public void updateCustomer(Model model) {
+		customerService.updateCustomer(model);
 	}
 
 }
