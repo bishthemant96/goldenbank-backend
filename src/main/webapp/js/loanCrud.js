9c3c9acbafe664a85bb09d9299b2e644912b7832
unknown <bharat.gupta@del1-lhp-n65278.synapse.com>
2019-08-28 11:36:50 +0530

    validation in loan module

--- src/main/webapp/js/loanCrud.js
+++ src/main/webapp/js/loanCrud.js
@@ -1,130 +1,89 @@
-window.onload = function(){
-	document.getElementById('div1').style.display="block";
-	document.getElementById('div3').style.display="none";
-	document.getElementById('div4').style.display="none";
-	
-	
+window.onload = function() {
+	document.getElementById('div1').style.display = "block";
+	document.getElementById('div3').style.display = "none";
+	document.getElementById('div4').style.display = "none";
+
 	var status = document.getElementById('status').value;
 	var error = document.getElementById('error').value;
-	if(status.localeCompare('false')==0){
-		alert("Operation failed with error:\n"+error);
-	} else if(status.localeCompare('true')==1){
+	if (status.localeCompare('false') == 0) {
+		alert("Operation failed with error:\n" + error);
+	} else if (status.localeCompare('true') == 1) {
 		alert("Request submitted successfully.");
 	}
-	
+
 }
 
 function toggleFunction(event) {
 	var caller = event.target.name;
-	
+
 	var one = document.getElementById('div1');
 	var three = document.getElementById('div3');
 	var four = document.getElementById('div4');
-	
-	
-	if(caller=="add"){
+
+	if (caller == "add") {
 		one.style.display = 'block';
 		three.style.display = 'none';
 		four.style.display = 'none';
-	} else if(caller=="delete"){
-        one.style.display = 'none';
-        three.style.display = 'block';
-        four.style.display = 'none';
-	} else if(caller=="view"){
-        one.style.display = 'none';
-        three.style.display = 'none';
-        four.style.display = 'block';
+	} else if (caller == "delete") {
+		one.style.display = 'none';
+		three.style.display = 'block';
+		four.style.display = 'none';
+	} else if (caller == "view") {
+		one.style.display = 'none';
+		three.style.display = 'none';
+		four.style.display = 'block';
 	}
 }
 
+function validateform1() {
+	var id = document.myform1.cusID.value;
+	var branch = document.myform1.branch.value;
+	var loanType = document.myform1.loanType.value;
+	var amount = document.myform1.amount.value;
 
-
-function validateform1(){  
-	var id=document.myform1.cusID.value;  
-	var branch=document.myform1.branch.value;       
-	var loanType=document.myform1.loanType.value;
-	var amount=document.myform1.amount.value;
-	
 	var numbers = /^[0-9]+$/;
 	var letters = /^[A-Za-z]+$/;
-	
-	
-	if (branch==null || branch==""){  
-		alert("branch can't be blank");  
-		return false;  
-	} 
-	else {
-		if(branch.match(letters))
-		{
-			return true;
-		}
-		else
-		{
-			alert("branch should be a text field");
-			return false;
-		}
-	}
-	
-	
-	
-	if(id==null || id==""){  
-		alert("ID can't be empty");  
-		return false;
-	}
-	else{
-		if(id.match(numbers)){
-			return true;
-		}
-		else{
-			alert("ID should be a number");  
-			return false;
-		}
-	}
-		
-	
-	if(loanType==null || loanType==""){  
-		alert("LoanType is a required field");  
-		return false;
-		if(loanType.match(letters))
-		{
-			return true;
-		}
-		else
-		{
-			alert("loanType should be a text field");
-			return false;
-		}
-	}
 
-	else if(amount==null || amount==""){  
-		alert("amount can not be empty");  
+	if (!branch.match(letters)) {
+		alert("branch should be a text field");
 		return false;
-		if(amount.match(numbers) && amount>=2000){
-			return true;
-		}
-		else{
-			alert("amount should be a numeric value and minimum 2000 rs");  
-			return false;
-		}
-	}   	
+	} else if (!id.match(numbers)) {
+		alert("ID should be a number");
+		return false;
+	} else if (!loanType.match(letters)) {
+		alert("Loan Type should be alphabetic");
+		return false;
+	} else if (!amount.match(numbers)) {
+		alert("amount can not be empty");
+		return false;
+	} else if (amount < 100) {
+		alert("Minimun transaction value should be greater than Rs 100");
+		return false;
+	} else {
+		return true;
+	}
 }
 
+function validateform2() {
+	var id = document.myform2.loanAccNo.value;
+	var numbers = /^[0-9]+$/;
 
-function validateform2(){  
-	var id=document.myform2.loanAccNo.value;  
-	  
-	if (id==null || id==""){  
-	  alert("loanAccNo can't be blank");  
-	  return false;  
-	}  
-	}  
-
-function validateform3(){  
-	var id=document.myform3.loanAccNo.value;  
-	  
-	if (id==null || id==""){  
-	  alert("User loanAccNo can't be blank");  
-	  return false;  
-	}  
+	if (!id.match(numbers)) {
+		alert("Loan Account Number should be a numeric value");
+		return false;
+	} else {
+		return true;
 	}
+}
 
+function validateform3() {
+	var id = document.myform3.loanAccNo.value;
+	var numbers = /^[0-9]+$/;
+
+	if (!id.match(numbers)) {
+		alert("Loan Account Number should be a numeric value");
+		return false;
+	} else {
+		return true;
+	}
+}
